621afb7cb25d7f215146b95a2abfab4d03ce2de9
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-08-25 18:01:34 +0200

    7/7

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,30 @@
-import SingleBook from './SingleBook'
-import {Col, Row} from 'react-bootstrap'
-
-function BookList({category}) {
-    return(
-        <Row key={category.category}>
-            <Col  xs='12'>
-            <h1>{category.category}</h1>
-            </Col> 
-            {category.map(book => <SingleBook data={book}/>)}      
-        </Row>
-    )
+import React from "react";
+import SingleBook from "./SingleBook";
+import { Col, Row } from "react-bootstrap";
+import category from "../data/fantasy.json";
+class BookList extends React.Component {
+	state = {
+		selected: false,
+	};
+	render() {
+		return (
+			<Row key={category[0].category}>
+				<Col xs="12">
+					<h1>{category[0].category}</h1>
+				</Col>
+				{category.map((book) => (
+					<SingleBook
+						setSelected={(asin) => {
+							this.setState({ selected: asin });
+							console.log({ book });
+						}}
+						selected={this.state.selected}
+						book={book}
+					/>
+				))}
+			</Row>
+		);
+	}
 }
 
-export default BookList
+export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,33 +1,35 @@
-import { Component } from 'react';
-import {Card, ListGroup, ListGroupItem, Col} from 'react-bootstrap'
+import { Component } from "react";
+import { Card, ListGroup, ListGroupItem, Col } from "react-bootstrap";
 
 class SingleBook extends Component {
-    state = {
-        selected: null
-    }
-    select (event){
-        this.setState({
-            selected:event.target
-        })
-        // this.state.selected
-    }
-    render(){
-    return(
-        <Col onClick={(event)=>this.select(event)} xs='3 mb-3' key={this.props.data.asin}>
-            <Card className='h-100'>
-                <Card.Img variant="top" src={this.props.data.img} />
-                <Card.Body>
-                    <Card.Title>{this.props.data.title}</Card.Title>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>{this.props.data.category}</ListGroupItem>
-                    <ListGroupItem>{this.props.data.price}</ListGroupItem>
-                    <ListGroupItem>{this.props.data.asin}</ListGroupItem>
-                </ListGroup>
-            </Card>
-        </Col>
-    )
-    }
-    }
+	render() {
+		return (
+			<Col
+				onClick={() => this.props.setSelected(this.props.book.asin)}
+				xs="3 mb-3"
+				key={this.props.book.asin}
+				className={
+					this.props.selected === this.props.book.asin ? "bg-dark" : ""
+				}
+			>
+				<Card className="h-100">
+					<Card.Img variant="top" src={this.props.book.img} />
+					<Card.Body>
+						<Card.Title>{this.props.book.title}</Card.Title>
+					</Card.Body>
+					<ListGroup className="list-group-flush">
+						<ListGroupItem>{this.props.book.category}</ListGroupItem>
+						<ListGroupItem>{this.props.book.price}</ListGroupItem>
+						<ListGroupItem>
+							{this.props.book.asin}
 
-export default SingleBook
+							{this.props.selected === this.props.book.asin ? "selected" : ""}
+						</ListGroupItem>
+					</ListGroup>
+				</Card>
+			</Col>
+		);
+	}
+}
+
+export default SingleBook;
